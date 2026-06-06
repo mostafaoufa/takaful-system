0221ee76d19dba9305475b6d77cd28096438efc6
Mostafa Oufa <mostafa.oufa@gmail.com>
2026-06-06 15:36:59 +0400

    fix render route

--- backend/server.js
+++ backend/server.js
@@ -254,6 +254,8 @@ app.post('/approve', (req, res) => {
 /* =========================
    ✅ START SERVER
 ========================= */
-app.listen(3000, () => {
-    console.log("🚀 Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("🚀 Server running on port " + PORT);
 });
